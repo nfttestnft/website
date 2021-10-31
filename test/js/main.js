6c9eddae7b79a8d13ce81160b9892aa48893af15
nfttest <nfttest@protonmail.com>
2021-10-31 12:38:36 +0100

    Update main.js

--- test/js/main.js
+++ test/js/main.js
@@ -12,3 +12,32 @@ ethereum
     // eth_accounts will return an empty array.
     console.error(err);
   });
+  
+  
+/***********************************/
+/* Handle connecting, per EIP 1102 */
+/***********************************/
+
+// You should only attempt to connect in response to user interaction,
+// such as a button click. Otherwise, you're popup-spamming the user
+// like it's 1999.
+// If you can't retrieve the user's account(s), you should encourage the user
+// to initiate a connection attempt.
+document.getElementById('btn-connect').addEventListener("click", connect);
+
+function connect() {
+	console.log("Connect button clicked");
+
+	ethereum
+    .request({ method: 'eth_requestAccounts' })
+	//.then(function(result) {console.log(result)})
+    .catch((err) => {
+      if (err.code === 4001) {
+        // EIP-1193 userRejectedRequest error
+        // If this happens, the user rejected the connection request.
+        console.log('Please connect to MetaMask.');
+      } else {
+        console.error(err);
+      }
+    });
+}
